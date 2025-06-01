660d8e7dcc715f9d5e924887b59aead65a8146ba
T4pei <frans04102006@gmail.com>
2025-06-01 15:33:29 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import  Flask, flash, redirect, render_template, request, url_for, session
 from config import Config
+import sqlite3 as sql
 
 class App_Kursus:
     def __init__(self):
@@ -9,6 +10,14 @@ class App_Kursus:
         self.routes()
 
     def routes(self):
+        def query_db(username, password):
+            conn = sql.connect('uas_pbo.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+            result = cursor.fetchone()
+            conn.close()
+            return result
+
         @self.app.route('/testdb')
         def testdb():
             try:
@@ -21,18 +30,32 @@ class App_Kursus:
         def index():
             return render_template('Main_page.html')
 
-        @self.app.route('/login/', methods=['GET', 'POST'])
+        @self.app.route('/login/')
         def login():
             return render_template('login.html')
 
-        @self.app.route('/register/', methods=['GET', 'POST'])
-        def register():
+        @self.app.route('/login/process', methods=['GET', 'POST'])
+        def loginProcess():
+            username = request.form['username']
+            password = request.form['password']
+            data = query_db(username, password)
+            if data:
+                flash('Login successful!', 'success')
+                return redirect(url_for('index'))
+            else:
+                flash('Wrong username/password. Please check again!', 'failed')                
+
+        @self.app.route('/register/')
+        def regiester():
+            return render_template('register.html')
+
+        @self.app.route('/register/process', methods=['POST'])
+        def registerProcess():
             if request.method == 'POST':
                 username = request.form['username']
                 password = request.form['password']
                 flash('Registration successful!', 'success')
-                return redirect(url_for('login'))
-            return render_template('register.html')
+            return redirect(url_for('login'))
 
     def run(self):
         self.app.run(debug=True)
